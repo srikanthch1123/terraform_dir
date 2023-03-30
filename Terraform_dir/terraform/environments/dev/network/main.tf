21565755118eaacfa161c6e382cbfb64b0481150
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-03-30 20:52:43 +0530

    Delete Terraform_dir/terraform/environments/dev/network directory

--- Terraform_dir/terraform/environments/dev/network/main.tf
+++
@@ -1,9 +0,0 @@
-module "network" {
-  source = "../../../module/network"
-  resource_group_name_network = var.resource_group_name_network
-  location = var.location
-  virtual_network_name = var.virtual_network_name
-  vnet_address_space = var.vnet_address_space
-  subnet_name = var.subnet_name
-  subnet_address_prefixes = var.subnet_address_prefixes
-}
