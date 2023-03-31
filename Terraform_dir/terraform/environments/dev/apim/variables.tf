d43fb15e0ad4fce62ce08e4991ab83324e75088b
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-03-31 08:00:11 +0530

    Create variables.tf

---
+++ Terraform_dir/terraform/environments/dev/apim/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name_apim" {}
+
+variable "location" {}
+
+variable "apim_name" {}
+
+variable "company_name" {}
+
+variable "publisher_email" {}
+
+variable "sku_name_apim" {}
