162cb5eb75c33bf0d4d53b1aa480a564e9b2903d
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-05 01:16:26 +0530

    Delete Terraform_dir/terraform/environments/dev/apim directory

--- Terraform_dir/terraform/environments/dev/apim/main.tf
+++
@@ -1,9 +0,0 @@
-module "apim" {
-  source = "../../../module/apim"
-  resource_group_name_apim = var.resource_group_name_apim
-  location = var.location
-  apim_name = var.apim_name
-  company_name = var.company_name
-  publisher_email = var.publisher_email
-  sku_name_apim = var.sku_name_apim
-}
