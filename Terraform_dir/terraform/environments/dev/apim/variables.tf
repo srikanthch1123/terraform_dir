29586a5cd0fa2ab672b124be191ff078f0fff104
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-06 09:25:27 +0530

    Update variables.tf

--- Terraform_dir/terraform/environments/dev/apim/variables.tf
+++ Terraform_dir/terraform/environments/dev/apim/variables.tf
@@ -10,16 +10,16 @@ variable "publisher_email" {}
 
 variable "sku_name_apim" {}
 
-variable "virtual_network_name" {}
+# variable "virtual_network_name" {}
 
-variable "resource_group_name_network" {}
+# variable "resource_group_name_network" {}
 
-variable "subnet_snet_apim_ds_name" {}
+# variable "subnet_snet_apim_ds_name" {}
 
-variable "private_endpoint_apim_name" {}
+# variable "private_endpoint_apim_name" {}
 
-variable "apim_dns_zone_vnet_link_name" {}
+# variable "apim_dns_zone_vnet_link_name" {}
 
-variable "apim_dns_a_record_name" {}
+# variable "apim_dns_a_record_name" {}
 
-variable "apim_private_service_connection" {}
+# variable "apim_private_service_connection" {}
