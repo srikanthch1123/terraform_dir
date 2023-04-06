52c3fd8336b614ab9dd4f1bbcc0caa5fa3a1947a
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-06 09:25:00 +0530

    Update main.tf

--- Terraform_dir/terraform/environments/dev/apim/main.tf
+++ Terraform_dir/terraform/environments/dev/apim/main.tf
@@ -5,12 +5,12 @@ module "apim" {
   apim_name = var.apim_name
   company_name = var.company_name
   publisher_email = var.publisher_email
-  sku_name_apim = var.sku_name_apim
-  virtual_network_name = var.virtual_network_name
-  resource_group_name_network = var.resource_group_name_network
-  subnet_snet_apim_ds_name = var.subnet_snet_apim_ds_name
-  private_endpoint_apim_name = var.private_endpoint_apim_name
-  apim_dns_zone_vnet_link_name = var.apim_dns_zone_vnet_link_name
-  apim_dns_a_record_name = var.apim_dns_a_record_name
-  apim_private_service_connection = var.apim_private_service_connection
+#   sku_name_apim = var.sku_name_apim
+#   virtual_network_name = var.virtual_network_name
+#   resource_group_name_network = var.resource_group_name_network
+#   subnet_snet_apim_ds_name = var.subnet_snet_apim_ds_name
+#   private_endpoint_apim_name = var.private_endpoint_apim_name
+#   apim_dns_zone_vnet_link_name = var.apim_dns_zone_vnet_link_name
+#   apim_dns_a_record_name = var.apim_dns_a_record_name
+#   apim_private_service_connection = var.apim_private_service_connection
 }
