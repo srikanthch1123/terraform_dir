5824b37325da4be066360bab84ad1ba1d458d3cd
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-06 09:32:46 +0530

    Update variables.tf

--- Terraform_dir/terraform/environments/dev/keyvault/variables.tf
+++ Terraform_dir/terraform/environments/dev/keyvault/variables.tf
@@ -4,14 +4,14 @@ variable "location" {}
 
 variable "key_vault_name" {}
 
-variable "kv_endpoint_name" {}
+# variable "kv_endpoint_name" {}
 
-variable "kv_private_service_connection" {}
+# variable "kv_private_service_connection" {}
 
-variable "kv_dns_a_record_name" {}
+# variable "kv_dns_a_record_name" {}
 
-variable "virtual_network_name" {}
+# variable "virtual_network_name" {}
 
-variable "resource_group_name_network" {}
+# variable "resource_group_name_network" {}
 
-variable "subnet_snet_shared_ds_name" {}
+# variable "subnet_snet_shared_ds_name" {}
