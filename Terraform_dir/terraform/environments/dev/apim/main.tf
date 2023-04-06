c55cbb81d84773db7f74d77caec289bd3abd3c6d
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-06 12:11:57 +0530

    Update main.tf

--- Terraform_dir/terraform/environments/dev/apim/main.tf
+++ Terraform_dir/terraform/environments/dev/apim/main.tf
@@ -5,7 +5,7 @@ module "apim" {
   apim_name = var.apim_name
   company_name = var.company_name
   publisher_email = var.publisher_email
-#   sku_name_apim = var.sku_name_apim
+  sku_name_apim = var.sku_name_apim
 #   virtual_network_name = var.virtual_network_name
 #   resource_group_name_network = var.resource_group_name_network
 #   subnet_snet_apim_ds_name = var.subnet_snet_apim_ds_name
