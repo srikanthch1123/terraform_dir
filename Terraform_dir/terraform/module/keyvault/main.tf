9c6b20a9ee0708c1652011ae8dc2e877b578e814
srikanthch1123 <119439813+srikanthch1123@users.noreply.github.com>
2023-04-06 09:19:59 +0530

    Update main.tf

--- Terraform_dir/terraform/module/keyvault/main.tf
+++ Terraform_dir/terraform/module/keyvault/main.tf
@@ -1,19 +1,19 @@
 data "azurerm_client_config" "current" {}
 
-data "azurerm_resource_group" "networkds" {
-  name     = var.resource_group_name_network
-}
+# data "azurerm_resource_group" "networkds" {
+#   name     = var.resource_group_name_network
+# }
 
-data "azurerm_virtual_network" "vnetds" {
-  name                = var.virtual_network_name
-  resource_group_name = data.azurerm_resource_group.networkds.name
-}
+# data "azurerm_virtual_network" "vnetds" {
+#   name                = var.virtual_network_name
+#   resource_group_name = data.azurerm_resource_group.networkds.name
+# }
 
-data "azurerm_subnet" "snet_shared_ds" {
-  name                 = var.subnet_snet_shared_ds_name
-  virtual_network_name = data.azurerm_virtual_network.vnetds.name
-  resource_group_name  = data.azurerm_resource_group.networkds.name
-}
+# data "azurerm_subnet" "snet_shared_ds" {
+#   name                 = var.subnet_snet_shared_ds_name
+#   virtual_network_name = data.azurerm_virtual_network.vnetds.name
+#   resource_group_name  = data.azurerm_resource_group.networkds.name
+# }
 
 resource "azurerm_resource_group" "keyvault" {
   name     = var.resource_group_name_kv
@@ -48,31 +48,31 @@ resource "azurerm_key_vault" "kv" {
   }
 }
 
-resource "azurerm_private_endpoint" "kv_endpoint" {
-  name                = var.kv_endpoint_name
-  location            = azurerm_resource_group.keyvault.location
-  resource_group_name = azurerm_resource_group.keyvault.name
-  subnet_id           = data.azurerm_subnet.snet_shared_ds.id
+# resource "azurerm_private_endpoint" "kv_endpoint" {
+#   name                = var.kv_endpoint_name
+#   location            = azurerm_resource_group.keyvault.location
+#   resource_group_name = azurerm_resource_group.keyvault.name
+#   subnet_id           = data.azurerm_subnet.snet_shared_ds.id
 
-  private_service_connection {
-    name                           = var.kv_private_service_connection
-    private_connection_resource_id = azurerm_key_vault.kv.id
-    is_manual_connection           = false
-    subresource_names = ["Vault"]
-  }
-}
-resource "azurerm_private_dns_zone" "dns_kv" {
-  name                = "privatelink.vaultcore.azure.net"
-  resource_group_name = azurerm_resource_group.keyvault.name
-}
-resource "azurerm_private_dns_a_record" "kv_dns_a_record" {
-  name                = var.kv_dns_a_record_name
-  zone_name           = azurerm_private_dns_zone.dns_kv.name
-  resource_group_name = azurerm_resource_group.keyvault.name
-  ttl                 = 300
-  records             = ["10.0.2.22"]
-}
+#   private_service_connection {
+#     name                           = var.kv_private_service_connection
+#     private_connection_resource_id = azurerm_key_vault.kv.id
+#     is_manual_connection           = false
+#     subresource_names = ["Vault"]
+#   }
+# }
+# resource "azurerm_private_dns_zone" "dns_kv" {
+#   name                = "privatelink.vaultcore.azure.net"
+#   resource_group_name = azurerm_resource_group.keyvault.name
+# }
+# resource "azurerm_private_dns_a_record" "kv_dns_a_record" {
+#   name                = var.kv_dns_a_record_name
+#   zone_name           = azurerm_private_dns_zone.dns_kv.name
+#   resource_group_name = azurerm_resource_group.keyvault.name
+#   ttl                 = 300
+#   records             = ["10.0.2.22"]
+# }
 
-output kv_private_ip {
-  value =   ["10.0.2.22"]
-}
+# output kv_private_ip {
+#   value =   ["10.0.2.22"]
+# }
